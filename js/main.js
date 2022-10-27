e17a717670c5ab78ec080d11df68ad747883f26d
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2022-10-27 15:05:55 +0200

    Aggiunge logica e stampa della lista su pagina

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 "use strict";
+
+const products = ["Carne", "Pesce", "Acqua","Birra", "Vino" ];
+
+const shoppingList = document.getElementById("shopping-list");
+
+let currentIndex = 0;
+
+while(currentIndex <= products.length -1){
+
+    let item = document.createElement("li");
+
+    item.innerHTML = products[currentIndex];
+
+    shoppingList.append(item);
+
+    currentIndex++;
+}
